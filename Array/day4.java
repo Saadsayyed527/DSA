377e3bdb38cbc9047884649c571aff2a095b3f12
Saadsayyed527 <137900721+Saadsayyed527@users.noreply.github.com>
2025-01-07 23:40:41 +0530

    day 5

--- Array/day4.java
+++ Array/day4.java
@@ -1,5 +1,6 @@
 public class day4 {
     public static void main(String[] args) {
-        System.out.println("hello day 4");
+        // System.out.println("hello day 4");
+        // day 4  sliding window questions and 5 questions each
     }
 }
--- Array/day5.java
+++ Array/day5.java
@@ -1,6 +1,7 @@
 public class day5 {
     public static void main(String[] args) {
         System.out.println("day 5");
+        System.out.println("starting advance");
         //  starting 5 
     }
 }
